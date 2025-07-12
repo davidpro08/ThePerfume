1eedcd99542cf81d153fcd0b6d74a09830cb40f6
GaYeon421 <dalneem3@gmail.com>
2025-07-12 13:18:51 +0900

    Update Player.cs
    달리기(LeftShift) 추가

--- Assets/Scirpt/Player.cs
+++ Assets/Scirpt/Player.cs
@@ -3,7 +3,10 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5f; // 기본 속도
+    private float runSpeed = 7f; // 달리기 속도
+    private bool isRunning = false; // 달리기 여부 (LeftShift)
+
     private Vector2 moveInput;
     private Rigidbody2D rb;
 
@@ -19,7 +22,12 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 movement = moveInput * moveSpeed;
+        Vector2 movement = isRunning ? runSpeed * moveInput : moveSpeed * moveInput;
         rb.linearVelocity = movement;
+    }
+
+    void Update()
+    {
+        isRunning = Keyboard.current.leftShiftKey.isPressed;
     }
 }
