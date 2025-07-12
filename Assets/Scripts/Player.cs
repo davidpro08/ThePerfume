f47a38a283ee3fb31e89bfa7d2be06662b0f856c
GaYeon421 <dalneem3@gmail.com>
2025-07-12 22:45:25 +0900

    달리기
    여기에 애니메이션을 넣으셨길래 옮겨왔습니다

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,13 @@ public class Player : MonoBehaviour
 {
     [Header("기본 설정")]
     public float moveSpeed = 5f;
-    
+
     [Header("인벤토리 설정")]
     public InventoryUIManager inventoryUIManager; // 연결할 인벤토리 UI 관리자
 
     private Vector2 moveInput;
+    private bool isRunning;
+    private float runRate = 1.8f; // 걷는 속력과 비교한 달리기 속력비
     private Rigidbody2D rb;
     private Animator animator;
 
@@ -48,7 +50,11 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 movement = moveInput * moveSpeed;
+        Vector2 movement = isRunning ? moveInput * moveSpeed * runRate : moveInput * moveSpeed;
         rb.linearVelocity = movement;
+    }
+    void Update()
+    {
+        isRunning = Keyboard.current.leftShiftKey.isPressed;
     }
 }
