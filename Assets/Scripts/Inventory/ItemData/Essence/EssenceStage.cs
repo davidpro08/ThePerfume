880ca065f50ae704d9b0062c297091b19ecd2689
GaYeon421 <dalneem3@gmail.com>
2025-08-18 22:06:21 +0900

    스프라이트 적용 전
    >> Fuel 적용하는데 수정 필요함

--- Assets/Scripts/Inventory/ItemData/Essence/EssenceStage.cs
+++ Assets/Scripts/Inventory/ItemData/Essence/EssenceStage.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-// 각 작물의 성장 단계 정보를 담고 있음
-
-[CreateAssetMenu(fileName = "CropStages", menuName = "Stage/Crop Stage")]
-public class CropStage : ScriptableObject
+[CreateAssetMenu(fileName = "EssenceStages", menuName = "Stage/EssenceStage")]
+public class EssenceStage : ScriptableObject
 {
-    [Header("성장 단계 설정")]
+    [Header("진행 단계 설정")]
     public List<Sprite> growthStage; // 각 성장 단계 스프라이트 설정
-    public int growDuration = 40; // 각 단계 별 걸리는 시간
+    public int growDuration = 10; // 각 단계 별 걸리는 시간
     public int totalStage => growthStage.Count; // 총 단계 개수
     public int fullGrowthIndex => growthStage.Count - 1; // 다 자란 단계 (마지막 단계)
 
