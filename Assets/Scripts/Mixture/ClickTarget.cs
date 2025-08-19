1227e7e9fe6a6ac6d0e9f30be4552fd9fc38af87
GaYeon421 <dalneem3@gmail.com>
2025-08-19 17:20:42 +0900

    중간 저장 > distiller 수정하러 갑니다

---
+++ Assets/Scripts/Mixture/ClickTarget.cs
@@ -0,0 +1,81 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider2D))]
+public class ClickTarget : MonoBehaviour, IPointerClickHandler, IDragHandler, IEndDragHandler
+{
+    public enum TargetType { Base, Middle, Top, Perfume, Funnel }
+    [SerializeField] TargetType type;
+    Mixture mixture;
+
+    void Awake()
+    {
+        mixture = GetComponent<Mixture>();
+        if (mixture == null) Debug.Log("[ClickTarget] 부모에 Mixture 없음");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+        if (eventData.pointerPressRaycast.module is not Physics2DRaycaster) return;
+        if (InventoryUIManager.isFullInventoryOpen || (TillUIManager.Instance != null && TillUIManager.Instance.isWarningCanvasOpen)) return;
+
+        switch (type)
+        {
+            case TargetType.Base:
+                // 아이템 소환
+                break;
+            case TargetType.Middle:
+                // 아이템 소환
+                break;
+            case TargetType.Top:
+                // 아이템 소환
+                break;
+            case TargetType.Perfume:
+                // 향수가 완성됐는지 확인
+                // 아이템 획득
+                break;
+            case TargetType.Funnel:
+                // 베이스,미들,탑 다 들어갔는지 체크
+                break;
+        }
+    }
+
+    public void OnDrag(PointerEventData evenetData)
+    {
+        switch (type)
+        {
+            case TargetType.Base:
+            case TargetType.Middle:
+            case TargetType.Top:
+                // 마우스 따라가기
+                break;
+            case TargetType.Perfume:
+                // 흔들 수 있는지 판단 후(깔떼기가 있는지 없는지)에 마우스 따라가기
+                break;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData evenetData)
+    {
+        switch (type)
+        {
+            case TargetType.Base:
+                // 위치가 '퍼퓸' 일 때
+                // 조건 판단 > 베이스가 없는지
+                // 퍼퓸관에 베이스 넣기
+                break;
+            case TargetType.Middle:
+                // 위치가 '퍼퓸' 일 때
+                // 조건 판단 > 베이스가 이미 있고 미들이 없는지
+                // 퍼퓸관에 미들 넣기
+                break;
+            case TargetType.Top:
+                // 위치가 '퍼퓸' 일 때
+                // 조건 판단 > 베이스, 미들이 이미 있고 탑이 없는지
+                // 퍼퓸관에 탑 넣기
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Mixture/Mixture.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Mixture : MonoBehaviour
+{
+    [Header("Slots")]
+    [SerializeField] Transform baseTransform;
+    [SerializeField] Transform middleTransform;
+    [SerializeField] Transform topTransform;
+    [SerializeField] Transform essenceTransform;
+    [SerializeField] Transform funnelTransform;
+}
