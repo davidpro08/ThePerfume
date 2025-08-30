7499d0e70e0d97c3da8846b5970abc3bdb687fa0
davidpro08 <davidmoon0817@gmail.com>
2025-08-30 22:26:00 +0900

    feat(tutorialManager): 튜토리얼 매니저 작동 잘 되는지 확인

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,33 @@
+﻿
+    using System.Collections;
+    using UnityEngine;
+
+    public class TutorialManager : MonoBehaviour
+    {
+        public static TutorialManager Instance;
+        public Npc guide;
+        void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Start()
+        {
+            // TODO: 나중에 실제 튜토리얼 했는지 파악하는거로 바꿔야함
+            if (true)
+            {
+                Debug.Log("튜토리얼 시작");
+                
+                StartCoroutine(StartTutorial());
+            }
+        }
+
+        IEnumerator StartTutorial()
+        {
+            yield return new WaitForSeconds(1f);
+            NpcDialogueManager.Instance.StartDialogue(guide);
+        }
+    }
